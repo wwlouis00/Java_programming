8847a4c7f441343fe892043b42ada30e084de967
wwlouis00 <64067917+wwlouis00@users.noreply.github.com>
2021-09-29 01:38:33 +0800

    Add files via upload

---
+++ 資料型別與運算子/ex02_01.java
@@ -0,0 +1,12 @@
+package ex02;
+
+public class ex02_01 {
+
+	public static void main(String[] args) {
+		System.out.println(" 짷턨⑧ 10:" + 10);
+		System.out.println(" 짨턨⑧ 10:" + 0b10);
+		System.out.println(" 짮턨⑧ 10:" + 010);
+		System.out.println(" 짷ㅋ턨⑧ 10:" + 0X10);
+	}
+
+}
