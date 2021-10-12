b71b3f3030b47dc9c8bed2de7981402f4aa3caa4
wwlouis00 <64067917+wwlouis00@users.noreply.github.com>
2021-10-12 21:30:47 +0800

    Update 20211012.java

--- NTUT/20211012.java
+++ NTUT/20211012.java
@@ -1,1 +1,21 @@
-
+public class ntut1012 {
+	public static void test(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i = 1;i<=10;i++)
+		{
+			for(int j = 1;j<=10;j++)
+			{
+				System.out.printf("%d*%d=%2d\n",i,j,i*j);
+				
+			}
+		}
+	}
+	public static void main(String[] argv) {
+		String a = "我正在學習java程式語言";
+		int square = 6;
+		int 面積 = square*square;
+		System.out.println("正方形面積是"+square*square);
+		System.out.println(面積);
+		
+	  }
+}
